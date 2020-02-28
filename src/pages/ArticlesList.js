23b9f67cde4300cb0a2580e11d18ee1eb14e462e
James Morris <blondfro@gmail.com>
2020-02-28 17:14:28 -0600

    created a list of articles from base data.

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,9 +1,19 @@
 import React from "react";
+import articleContent from "./article-content";
+import {Link} from "react-router-dom";
 
 const ArticlesList = () => {
     return (
         <>
             <h1>Articles List</h1>
+            {articleContent.map(article => (
+                <Link key={article.name}
+                      className="article-list-item"
+                      to={'article/' + article.name}>
+                    <h3>{article.title}</h3>
+                    <p>{article.content[0].substring(0, 150)}...</p>
+                </Link>
+            ))}
         </>
     )
 };
