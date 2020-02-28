55e633218277df84b9391f6d2365476f7e3cfd3f
James Morris <blondfro@gmail.com>
2020-02-28 16:25:26 -0600

    created a basic homepage and then modified the app.css based on the training files.

---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Homepage = () => {
+    return (
+        <>
+            <h1>Hello, welcome to my blog</h1>
+            <p>
+                Welcome to my blog!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, assumenda culpa eaque et hic illum impedit ipsum laboriosam nisi nulla obcaecati perspiciatis porro quam quasi quidem recusandae similique soluta voluptates?  Cum doloremque minima optio quo voluptas. Accusantium, alias amet consequuntur cumque delectus deserunt dolores earum, eum eveniet exercitationem harum ipsum molestias necessitatibus perspiciatis placeat quae, qui quidem quo suscipit tenetur?
+        </p>
+            <p>
+                <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque facere laudantium magni non praesentium, quidem vel. Aliquid autem blanditiis consectetur eligendi eveniet expedita magni perferendis quia recusandae soluta ut, vitae!>Aut ea enim eveniet expedita facere illum impedit ipsa iure maxime nesciunt odio officia optio perferendis possimus, quae quia rerum sapiente sit totam unde. Consequuntur est id in inventore mollitia!</span>
+            </p>
+        </>
+    )
+};
+
+export default Homepage;
