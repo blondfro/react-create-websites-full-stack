5d852e5678c020cf2992f25c4216265a49584c69
James Morris <blondfro@gmail.com>
2020-02-28 16:44:32 -0600

    created a shells for about, articles and articles lists.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom"
 
 import './App.css';
-import Homepage from "./pages/HomePage";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesList from "./pages/ArticlesList";
+import ArticlePage from "./pages/ArticlePage";
 
 function App() {
   return (
       <Router>
         <div className="App">
-          <Route exact path="/" component={Homepage} />
+            <div id="page-body">
+                <Route exact path="/" component={HomePage} />
+                <Route path="/about" component={AboutPage} />
+                <Route path="/articles-list" component={ArticlesList} />
+                <Route path="/article" component={ArticlePage} />
+            </div>
         </div>
       </Router>
   );
---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ArticlePage = () => {
+    return (
+        <>
+            <h1>This is an Article</h1>
+        </>
+    )
+};
+
+export default ArticlePage;
