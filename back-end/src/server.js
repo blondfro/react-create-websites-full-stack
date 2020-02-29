468ba80dd5ed0d3c092194a2434a12d80b1422b0
James Morris <blondfro@gmail.com>
2020-02-29 13:43:57 -0600

    created a basic server.js file and created a babelrc file.

---
+++ back-end/src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+
+
+app.get("/hello", (req, res) => {
+    res.send("Hello");
+});
+
+app.listen(PORT, ()=>{console.log("Listening on port: ", PORT)});
