2a3f85251796c8c60f67c38435fa33b2e3c45718
James Morris <blondfro@gmail.com>
2020-02-29 14:15:00 -0600

    created an up vote route and some simple test data.

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,21 +1,31 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+const articlesInfo = {
+    "learn-react": {
+        upvotes: 0
+    },
+    "learn-node": {
+        upvotes: 0
+    },
+    "my-thoughts-on-resumes": {
+        upvotes: 0
+    }
+};
+
 const app = express();
 const PORT = 8000;
 
 app.use(bodyParser.json());
 
-app.get("/hello", (req, res) => {
-    res.send("Hello");
-});
+app.post("/api/articles/:name/upvote", (req, res) => {
+    const articleName = req.params.name;
 
-app.get("/hello/:name", (req, res)=>{
-    res.send("Hello " + req.params.name);
-});
-
-app.post("/hello", (req, res) => {
-    res.send("Hello " + req.body.name);
+    articlesInfo[articleName].upvotes += 1;
+    res.status(200).send(articleName +
+        ' now has ' +
+        articlesInfo[articleName].upvotes +
+        ' votes');
 });
 
 app.listen(PORT, ()=>{console.log("Listening on port: ", PORT)});
