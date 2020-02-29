3ff8069eddbb5dc763617b7ecb978ca71c9f6f41
James Morris <blondfro@gmail.com>
2020-02-29 16:38:29 -0600

    installed mongodb and connected a get request to get a specific article.

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,25 +1,34 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { MongoClient } from "mongodb";
 
-const articlesInfo = {
-    "learn-react": {
-        upvotes: 0,
-        comments: []
-    },
-    "learn-node": {
-        upvotes: 0,
-        comments: []
-    },
-    "my-thoughts-on-resumes": {
-        upvotes: 0,
-        comments: []
-    }
-};
 
 const app = express();
 const PORT = 8000;
 
+
+
 app.use(bodyParser.json());
+
+app.get("/api/articles/:name", async (req, res) => {
+    try {
+        const articleName = req.params.name;
+        const client = await MongoClient.connect('mongodb://localhost:27017', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        const db = client.db('my-blog');
+
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(articleInfo);
+        client.close();
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error connecting to database',
+            error})
+    }
+    
+});
 
 app.post("/api/articles/:name/upvote", (req, res) => {
     const articleName = req.params.name;
