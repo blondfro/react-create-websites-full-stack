d45c58e54653d59d3b9ae9b53d58dc5787f1d285
James Morris <blondfro@gmail.com>
2020-02-29 14:02:22 -0600

    installed body-parser and setup a simple test post route.

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,11 +1,17 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 8000;
 
+app.use(bodyParser.json());
 
 app.get("/hello", (req, res) => {
     res.send("Hello");
+});
+
+app.post("/hello", (req, res) => {
+    res.send("Hello " + req.body.name);
 });
 
 app.listen(PORT, ()=>{console.log("Listening on port: ", PORT)});
